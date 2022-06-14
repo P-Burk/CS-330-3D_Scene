411f93aee17e66dcb2f9763334edc7867088b6c6
Preston <68164093+P-Burk@users.noreply.github.com>
2022-06-14 16:31:07 -0400

    Create light_cube.fs

---
+++ include/light_cube.fs
@@ -0,0 +1,15 @@
+/****** CODE CITATION **************************************************************
+* Title: Learn OpenGL: 6.light_cube.fs
+* Author: Joey de Vries
+* Date Accessed: 14 June 2022
+* Code Version: N/A
+* Availability: https://learnopengl.com/code_viewer_gh.php?code=src/2.lighting/6.multiple_lights/6.light_cube.fs
+************************************************************************************/
+
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0); // set alle 4 vector values to 1.0
+}
